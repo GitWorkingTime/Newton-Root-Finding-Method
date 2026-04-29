e59285505c23cfcc15f75eb3e91189842062dcb4
GitWorkingTime <dlee28262@gmail.com>
2026-04-29 11:09:31 -0600

    Implemented recursive function that follows newton's method

--- src/newton's_method.ts
+++ src/newton's_method.ts
@@ -1,24 +1,31 @@
-import {derivative, evaluate, MathNode, parse} from 'mathjs'
+import {den, derivative, evaluate, MathNode, parse} from 'mathjs'
 /* 
 let f(x) be some continuous, differentiable function
 Newton's method of Root finding is a recursive function that uses the following formula:
 
 x_(n+1) = x_n - (f(x_n))/(f'(x_n))
 */
+interface xCoord {
+    x: number;
+};
 
 // For now, let's use a predefined polynomial
-let expr: string = 'x^2 + 2x - 8';
+let expr: string = 'e^x - 2';
 let f = parse(expr);
 
-// We can find the derivative using the math.js library
-let dfExpr = derivative(f, 'x').toString();
-let df = parse(dfExpr);
+function rootFinding(x: xCoord, iterations: number, expr: MathNode): number {
+    if (iterations <= 0) {
+        return x.x;
+    } else {
+        let dexpr = derivative(expr, 'x');
+        let numerator = expr.evaluate(x);
+        let denominator = dexpr.evaluate(x);
+        let root = x.x - (numerator/denominator);
+        let newX: xCoord = {x: root};
+        return rootFinding(newX, iterations - 1, expr);
+    }
+}
 
-console.log(`f: ${f}`);
-console.log(`df: ${df}`);
+let val: xCoord = {x: 1};
 
-let xCoord = {x: 1};
-let numerator = f.evaluate(xCoord);
-let denominator = df.evaluate(xCoord);
-let root = xCoord.x - numerator/denominator;
-console.log(root);
+console.log(rootFinding(val, 5, f));
